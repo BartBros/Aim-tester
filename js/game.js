cbad1905f7d53f98f6d6c78bb540afe3ff1089d8
BartBros <bb.aambroziak@gmail.com>
2019-05-13 18:36:21 +0200

    add test game over

--- js/game.js
+++ js/game.js
@@ -3,20 +3,38 @@ const table = $('table');
 const dot = document.createElement('button');
 $(dot).addClass('target');
 
+// Clear Window in Game
+const clearWindow = document.createElement('div');
+$(clearWindow).addClass('d-flex flex-column justify-content-center align-items-center w-100 h-100');
+
 let points = 0;
 let $position = $('td');
 
 let i = 0;
 
-$position[i].append(dot);
+
+// Generate random position for game target
+function positionRandom() {
+    return Math.floor(Math.random() * $position.length) + 1;
+  }
+
+// First position of target
+let positionStart = positionRandom();
+$position[positionStart].append(dot);
 
 $('tr').on('click', function(event) {
     if (event.target.tagName == 'BUTTON') {
         $(event.target).remove();
-        i++;
         points++;
+        let posRandom = positionRandom();
         $('#div-points').text(points);
-        $position[i].append(dot);
+        $position[posRandom].append(dot);
+    } if (event.target.tagName == 'TD') {
+        points = 0;
+        $('#div-points').text(points);
+        $('table').remove();
+        $('#main-game').append(clearWindow);
+        $(clearWindow).append('<p>test</p>');
 
-}
+    }
 });
