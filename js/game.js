a4322077d104eeabba62bc0cdb5c856615bc372b
BartBros <bb.aambroziak@gmail.com>
2019-05-14 13:48:48 +0200

    add simple images

--- js/game.js
+++ js/game.js
@@ -1,24 +1,69 @@
 
-const table = $('table');
-const dot = document.createElement('button');
-$(dot).addClass('target');
+// ========= GAME VARIABLES =========
+let points;
+let shots;
+let bullets;
+let topScore = 0;
+
+
+const duck = document.createElement('img');
+$(duck).attr('id', 'duck');
+$(duck).attr('src', '../img/duck_outline_target_yellow.png');
+$(duck).width(50).height(50);
+
+// Game field
+const gameField = document.createElement('table');
+$(gameField).addClass('table w-100 h-100').addClass('pistol');
+
+for (let i = 0; i < 9; i++) {
+    const tableRow = document.createElement('tr');
+    $(gameField).append(tableRow);
+    for (let i = 0; i < 14; i++) {
+        const tableCell = document.createElement('td');
+        $(tableRow).append(tableCell);
+    }
+}
+
 
 // Clear Window in Game
 const clearWindow = document.createElement('div');
-clearWindow.classList.add('d-flex', 'flex-column', 'justify-content-center', 'align-items-center', 'w-100', 'h-100');
-clearWindow.textContent = 'tekst';
+$(clearWindow).addClass('d-flex flex-column justify-content-center align-items-center w-100 h-100 bg-empty');
+
+
+// ========= FUNCTIONS =========
+function startGame() {
+    $('#main-game').empty();
+    $('#main-game').append(gameField);
+    $(clearWindow).empty();
+    points = 0;
+    $('#points').text(points);
+}
+
 
 // ========= TEXT NOTIFICATIONS =========
-// const tEmpty = document.createElement('p');
-// const tGameOver = document.createTextNode('Game Over');
 
-// tEmpty.append(tGameOver);
+// Game Over
+const nGameOver = document.createElement('p');
+$(nGameOver).css('color','red').css('text-shadow', '1px 1px 2px rgb(0, 0, 0)');
+$(nGameOver).text('Game Over');
 
+// Try Again
+const nTryAgain = document.createElement('p');
+$(nTryAgain).css('color','green').css('text-shadow', '1px 1px 2px rgb(0, 0, 0)');
+$(nTryAgain).addClass('pointer');
+$(nTryAgain).text('Try Again');
 
-let points = 0;
-let $position = $('td');
+// Game Over
+const nTopScore = document.createElement('p');
+$(nTopScore).css('color','rgb(255, 230, 0)').css('text-shadow', '1px 1px 2px rgb(0, 0, 0)');
+$(nTopScore).text('Congratulations! You Gained new Top Score!');
 
-let i = 0;
+// ========= MAIN GAME ACTIONS =========
+
+startGame();
+
+const table = $('table');   // Whole table is playing field
+let $position = $('td');    // Table cell is position for target
 
 
 // Generate random position for game target
@@ -28,21 +73,36 @@ function positionRandom() {
 
 // First position of target
 let positionStart = positionRandom();
-$position[positionStart].append(dot);
+$position[positionStart].append(duck);
 
-$('tr').on('click', function(event) {
-    if (event.target.tagName == 'BUTTON') {
+$('#main-game').on('click', function(event) {               // Target hit correct
+    if (event.target.id == 'duck') {
         $(event.target).remove();
         points++;
         let posRandom = positionRandom();
-        $('#div-points').text(points);
-        $position[posRandom].append(dot);
-    } if (event.target.tagName == 'TD') {
-        points = 0;
-        $('#div-points').text(points);
+        $('#points').text(points);
+        $position[posRandom].append(duck);
+
+    } if (event.target.tagName == 'TD') {           // Hit outside the target
         $('table').remove();
         $('#main-game').append(clearWindow);
-        $(clearWindow).append('<p>test</p>');
-
+        if (points > topScore) {
+            $(clearWindow).append(nGameOver)
+            .append(nTopScore)
+            .append(nTryAgain);   
+            topScore = points;
+            $('#top-score').text(topScore);
+        } else {
+            $(clearWindow).append(nGameOver)
+            .append(nTryAgain);
+        }
     }
+});
+
+// ========= MENU ELEMENTS =========
+
+$('#main-game').on('click', function(event) {
+    if (event.target == nTryAgain) {
+        startGame();
+    }  
 });
