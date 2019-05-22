9ecea81e991fa6b580f6409ead32988147faf9b7
BartBros <bb.aambroziak@gmail.com>
2019-05-22 13:30:34 +0200

    fix sounds

--- js/sounds.js
+++ js/sounds.js
@@ -1,31 +1,37 @@
 const soundStart = document.createElement("audio");
 soundStart.src = "../Aim-tester/sounds/start.wav";
 soundStart.volume = 1;
+soundStart.preLoad = true;
 soundStart.autoPlay = false;
 
 const soundDuck = document.createElement("audio");
 soundDuck.src = "../Aim-tester/sounds/duck.wav";
 soundDuck.volume = 1;
+soundStart.preLoad = true;
 soundDuck.autoPlay = false;
 
 const soundShot = document.createElement("audio");
 soundShot.src = "../Aim-tester/sounds/shot.wav";
 soundShot.volume = 1;
+soundStart.preLoad = true;
 soundShot.autoPlay = false;
 
 const soundFlew = document.createElement("audio");
 soundFlew.src = "../Aim-tester/sounds/flew.wav";
 soundFlew.volume = 1;
+soundStart.preLoad = true;
 soundFlew.autoPlay = false;
 
 const soundTopScore = document.createElement("audio");
 soundTopScore.src = "../Aim-tester/sounds/topscore.wav";
 soundTopScore.volume = 1;
+soundStart.preLoad = true;
 soundTopScore.autoPlay = false;
 
 const soundGameOver = document.createElement("audio");
 soundGameOver.src = "../Aim-tester/sounds/gameover.wav";
 soundGameOver.volume = 1;
+soundStart.preLoad = true;
 soundGameOver.autoPlay = false;
 
 
